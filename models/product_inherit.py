21368af947c76e8aea98d0877d3d5ec95721a5c3
Wilson <wilsonhernandezortiz@gmail.com>
2025-11-15 19:01:27 -0500

    added date expiration picker

--- models/product_inherit.py
+++ models/product_inherit.py
@@ -1,7 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from datetime import date
 
 class ProductTemplateInherit(models.Model):
     _inherit = 'product.template'
+    future_date = fields.Date(string="Fecha futura")
 
-    pos_note = fields.Char(string="Nota para el POS")
+    @api.constrains('future_date')
+    def _check_future_date(self):
+        for record in self:
+            if record.future_date and record.future_date <= date.today():
+                raise ValidationError("La fecha debe ser posterior a hoy.")
+
+    #pos_note = fields.Char(string="Nota para el POS")
 
